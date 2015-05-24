bc203753f00167e40b596d5188dfd83d7722bc9c
fsahinbas <fsahinbas@gmail.com>
2015-05-24 22:00:37 +0300

    questions

--- iys/iys/Controllers/QuestionsController.cs
+++ iys/iys/Controllers/QuestionsController.cs
@@ -20,6 +20,11 @@ namespace iys.Controllers
             return View();
         }
 
+        public ActionResult Partial1()
+        {
+            return PartialView("Partial1");
+        }
+
         iys.ModelProject.iysContext db = new iys.ModelProject.iysContext();
 
         [ValidateInput(false)]
@@ -299,5 +304,7 @@ namespace iys.Controllers
             //int courseID = (Request.Params["COURSE_CODE"] != null) ? int.Parse(Request.Params["COURSE_CODE"]) : -1;
             return PartialView(getDocument(COURSE_CODE, CHAPTER_CODE, lessonID));
         }
+
+
     }
 }
