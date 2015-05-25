151faf91c5a0f1780e5bee310a73a5f5b99d7963
cantek41 <cantek41@gmail.com>
2015-05-25 21:13:39 +0300

    dashboard

---
+++ iys/iys/Controllers/DashboardController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevExpress.DashboardWeb.Mvc;
+using DevExpress.Web.Mvc;
+
+namespace iys.Controllers
+{
+    public class DashboardController : Controller
+    {
+        //
+        // GET: /Dashboard/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ValidateInput(false)]
+        public ActionResult DashboardViewerPartial()
+        {
+            return PartialView("_DashboardViewerPartial", DashboardViewerSettings.Model);
+        }
+        public FileStreamResult DashboardViewerPartialExport()
+        {
+            return DevExpress.DashboardWeb.Mvc.DashboardViewerExtension.Export("DashboardViewer", DashboardViewerSettings.Model);
+        }
+
+        iys.ModelProject.iysContext db = new iys.ModelProject.iysContext();
+
+        public ActionResult ChartPartial()
+        {
+            var model = db.USER_ANSWERS;
+            return PartialView("_ChartPartial", model.ToList());
+        }
+	}
+    class DashboardViewerSettings
+    {
+        public static DevExpress.DashboardWeb.Mvc.DashboardSourceModel Model
+        {
+            get
+            {
+                DashboardSourceModel model = new DashboardSourceModel();
+                model.DashboardSource=typeof(Dashboard1);
+                return model;
+            }
+        }
+    }
+
+}
---
+++ iys/iys/Dashboard1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace iys
+{
+    partial class Dashboard1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.DashboardCommon.DashboardLayoutGroup dashboardLayoutGroup1 = new DevExpress.DashboardCommon.DashboardLayoutGroup();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // Dashboard1
+            // 
+            dashboardLayoutGroup1.DashboardItem = null;
+            dashboardLayoutGroup1.Orientation = DevExpress.DashboardCommon.DashboardLayoutGroupOrientation.Vertical;
+            this.LayoutRoot = dashboardLayoutGroup1;
+            this.Title.ImageDataSerializable = "";
+            this.Title.Text = "Dashboard";
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+
+
+
+    }
+}
